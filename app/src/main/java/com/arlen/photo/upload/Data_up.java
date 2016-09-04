41e35d5e80c6d7c796e6e7bad9c447dd82724858
yubowengod <heiheijun@hotmail.com>
2016-09-04 11:18:25 +0800

    uploadPhoto_myapplication34_9.31

--- app/src/main/java/com/arlen/photo/upload/Data_up.java
+++ app/src/main/java/com/arlen/photo/upload/Data_up.java
@@ -6,7 +6,7 @@ package com.arlen.photo.upload;
 public class Data_up {
 //    private static String a ="feiyangxiaomi";
 
-    private static String SERVICE_URL = "http://10.148.83.157:8011/Service1.asmx";
+    private static String SERVICE_URL = "http://192.168.1.117:8011/Service1.asmx";
 
     public static String getSERVICE_URL() {
         return SERVICE_URL;
